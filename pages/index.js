05603294330346d51ce5a96e69464cf720603cd5
RISHABHsharm <rishabhsharma3108@gmail.com>
2024-03-11 20:23:13 +0530

    guess-result for given name

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,63 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import GuessResult from '../components/guess-results.component';
+import useAPI from '../custom-hooks/useAPI';
 
-export default function Home() {
+const URL_AGE =   "https://api.agify.io";
+const URL_GENDER = "https://api.genderize.io";
+const URL_NATION = "https://api.nationalize.io";
+
+const Home = () => {
+  const [value, setValue] = useState("");
+  const { data: ageData, loading: ageLoading, error: ageError, fetchData: fetchAge } = useAPI(URL_AGE);
+  const { data: genderData, loading: genderLoading, error: genderError, fetchData: fetchGender } = useAPI(URL_GENDER);
+  const { data: countryData, loading: countryLoading, error: countryError, fetchData: fetchCountry } = useAPI(URL_NATION);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(value.length < 3){
+      return;
+    };
+
+    fetchAge(value);
+    fetchGender(value);
+    fetchCountry(value);
+    setValue("");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Hy-vee Assignment</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h1>Guess the person by name</h1>
+        <input
+          required 
+          className={styles.guessInput}
+          placeholder="Enter search name"
+          value={value}
+          onChange={({target}) => setValue(target.value)}
+        />
+        <button 
+          type='submit' 
+          className={styles.submit}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+          {ageLoading || genderLoading || countryLoading ? "Searching..." : "Submit"}
+        </button>
+      </form>
+      {ageLoading || genderLoading || countryLoading ? 
+        <h2 className={styles.header}>loading...</h2> : 
+        <GuessResult 
+          guessData={{
+            data : {ageData, genderData, countryData},
+            error : {ageError, genderError, countryError}
+          }} 
+        />
+      }
     </div>
   );
-}
+};
+
+export default Home;
